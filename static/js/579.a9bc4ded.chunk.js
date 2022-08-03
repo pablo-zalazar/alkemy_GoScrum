841c3d254ebb101a725609f0092cea1738265992
Pablo-Zalazar <pablo_zalazar@outlook.com>
2022-08-03 12:58:32 -0300

    Updates

---
+++ static/js/579.a9bc4ded.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoscrum=self.webpackChunkgoscrum||[]).push([[579],{3579:function(n,c,e){e.r(c),e.d(c,{default:function(){return u}});var r=e(184);function u(){return(0,r.jsx)("div",{className:"container",children:"Pagina no encontrada"})}}}]);
+//# sourceMappingURL=579.a9bc4ded.chunk.js.map
